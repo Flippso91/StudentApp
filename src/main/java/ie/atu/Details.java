4c75852b0072faae004d7baf830720f2ca6cd663
Flippso91 <g00351833@atu.ie>
2022-10-24 17:25:45 +0100

    1st commit, created Details class and started scanner

---
+++ src/main/java/ie/atu/Details.java
@@ -0,0 +1,31 @@
+package ie.atu;
+
+public class Details {
+    private String studentName;
+    private String studentEmail;
+    private String studentCourse;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public String getStudentCourse() {
+        return studentCourse;
+    }
+
+    public void setStudentCourse(String studentCourse) {
+        this.studentCourse = studentCourse;
+    }
+}
---
+++ src/main/java/ie/atu/StudentApp.java
@@ -0,0 +1,11 @@
+package ie.atu;
+
+import java.util.Scanner;
+
+public class StudentApp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Details student1 = new Details();
+    }
+}
