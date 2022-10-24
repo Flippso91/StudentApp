bc6268e7c157ed1aec9730f3ea2abbe06b38166e
Flippso91 <g00351833@atu.ie>
2022-10-24 17:30:55 +0100

    Have 1st name entry working, can use getters and setters handily

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -7,5 +7,8 @@ public class StudentApp {
         Scanner input = new Scanner(System.in);
 
         Details student1 = new Details();
+        System.out.println("Please enter Student Name: ");
+        student1.setStudentName(input.next());
+        System.out.println("you entered: " + student1.getStudentName());
     }
 }
